05680fdefe876161dc9874e42216f1eb6d83a69b
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-08-06 19:05:55 -0500

    se suben pequeñas cosas

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         'NAME': 'tesis',
         'USER': 'root',
         'HOST': 'localhost',
-        'PORT': '3306',
-        'PASSWORD': '1616'
+        'PORT': '3307',
+        'PASSWORD': 'matz'
     }
 }
 
